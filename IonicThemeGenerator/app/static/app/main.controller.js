95a5b10a45647adbdc017a965d225e35cd488174
Kerry Ritter <kritter@swankmp.com>
2015-02-24 13:24:03 -0600

    the SASS is now compiling based on inputs.

---
+++ IonicThemeGenerator/app/static/app/main.controller.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('itgApp', []);
+
+app.controller("MainController", ["$scope", "$http", function($scope, $http, config) {
+    $scope.state = {
+        variables: window.ionicVariables
+    }
+
+    $scope.save = function() {
+        $http.post('/process', { variables: $scope.state.variables })
+        .success(function(data) {
+            console.log(data);
+        });
+    }
+}]);
--- IonicThemeGenerator/app/views.py
+++ IonicThemeGenerator/app/views.py
@@ -6,8 +6,13 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
+import json
 import sass
+import os
+import uuid
+import shutil
 
 def home(request):
     """Renders the home page."""
@@ -22,5 +27,38 @@ def home(request):
         })
     )
 
+@csrf_exempt
 def process(request):
-    return HttpResponse(sass.compile(filename='C:/Workspaces2/IonicThemeGenerator/IonicThemeGenerator/app/static/temp/ionic.scss'))
+    raw_request_body = request.body;
+    variables = json.loads(raw_request_body.decode("utf-8"))['variables']
+
+    ionic_path = write_variables_to_file(variables)
+
+    sass_result = sass.compile(filename=ionic_path + "ionic.scss")
+
+    file = open(ionic_path + 'output.css', 'w')
+    file.writelines(sass_result)
+
+    #shutil.rmtree(ionic_path)
+
+    return HttpResponse(sass_result)
+
+def write_variables_to_file(variables):
+    lines = []
+
+    id = str(uuid.uuid4())
+    temp_directory = "C:/Workspaces2/Ionic Theme Generator/IonicThemeGenerator/app/static/temp/"
+    directory = temp_directory + id + "/"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for variable in variables:
+        lines.append(variable['name'] + ": " + variable['value']  + " !default; \n")
+
+    file = open(directory + '_variables.scss', 'w')
+    file.writelines(lines)
+
+    shutil.copy(temp_directory + "ionic.scss", directory + "ionic.scss")
+
+    return directory
